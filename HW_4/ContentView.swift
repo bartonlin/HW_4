8ea93418c2563174fdaca84ad73f7bd6d081be52
HsiehYuHeng <k28998989@gmail.com>
2019-12-03 14:11:25 +0800

    Initial Commit

---
+++ HW_4/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HW_4
+//
+//  Created by User17 on 2019/12/3.
+//  Copyright © 2019 NTOU. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
